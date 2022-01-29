898efc418b27e543c5a504043873818f166dcfb8
zitianawa <98625952+zitianawa@users.noreply.github.com>
2022-01-29 11:30:04 +0800

    Delete js directory

--- js/component/PlayButton.js
+++
@@ -1,3 +0,0 @@
-CPWL.PlayButton = function (parent,clickFunc) {
-    CPWL.RotateButton.call(this,80,"assets/img/ui/playBg.png","assets/img/ui/play.png",parent,clickFunc);
-};
--- js/component/RightArrow.js
+++
@@ -1,5 +0,0 @@
-CPWL.RightArrow = function (parentEl,clickFunc) {
-    CPWL.RotateButton.call(this,48,"assets/img/ui/arrowBg.png","assets/img/ui/arrowRight.png",parentEl,clickFunc,{
-        right : "35px"
-    });
-};
